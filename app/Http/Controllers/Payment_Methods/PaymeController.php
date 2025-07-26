7c1b3987632b6e9f52dcfefe986c7e178b0b2bbd
gkurbanov <geldierkinovich@gmail.com>
2025-07-26 22:14:38 +0500

    fix: open payme link

--- app/Http/Controllers/Payment_Methods/PaymeController.php
+++ app/Http/Controllers/Payment_Methods/PaymeController.php
@@ -90,32 +90,32 @@ class PaymeController extends Controller
         $getUniqueId = OrderManager::generateUniqueOrderID();
 
         $orderIds = [];
-        foreach ($cartGroupIds as $groupId) {
-            $data = [
-                'payment_method' => 'cash_on_delivery',
-                'order_status' => 'pending',
-                'payment_status' => 'unpaid',
-                'transaction_ref' => '',
-                'order_group_id' => $getUniqueId,
-                'cart_group_id' => $groupId,
-                'request' => $request,
-                'newCustomerRegister' => $newCustomerRegister,
-                'bring_change_amount' => $request['bring_change_amount'] ?? 0,
-                'bring_change_amount_currency' => $currencyCode,
-            ];
+//        foreach ($cartGroupIds as $groupId) {
+//            $data = [
+//                'payment_method' => 'cash_on_delivery',
+//                'order_status' => 'pending',
+//                'payment_status' => 'unpaid',
+//                'transaction_ref' => '',
+//                'order_group_id' => $getUniqueId,
+//                'cart_group_id' => $groupId,
+//                'request' => $request,
+//                'newCustomerRegister' => $newCustomerRegister,
+//                'bring_change_amount' => $request['bring_change_amount'] ?? 0,
+//                'bring_change_amount_currency' => $currencyCode,
+//            ];
+//
+//            $orderId = OrderManager::generate_order($data);
+//
+//            $order = Order::find($orderId);
+//            $order->billing_address = ($request['billing_address_id'] != null) ? $request['billing_address_id'] : $order['billing_address'];
+//            $order->billing_address_data = ($request['billing_address_id'] != null) ? ShippingAddress::find($request['billing_address_id']) : $order['billing_address_data'];
+//            $order->order_note = ($request['order_note'] != null) ? $request['order_note'] : $order['order_note'];
+//            $order->save();
+//
+//            $orderIds[] = $orderId;
+//        }
 
-            $orderId = OrderManager::generate_order($data);
-
-            $order = Order::find($orderId);
-            $order->billing_address = ($request['billing_address_id'] != null) ? $request['billing_address_id'] : $order['billing_address'];
-            $order->billing_address_data = ($request['billing_address_id'] != null) ? ShippingAddress::find($request['billing_address_id']) : $order['billing_address_data'];
-            $order->order_note = ($request['order_note'] != null) ? $request['order_note'] : $order['order_note'];
-            $order->save();
-
-            $orderIds[] = $orderId;
-        }
-
-        CartManager::cart_clean($request);
+//        CartManager::cart_clean($request);
 
 //        // Continue with payment gateway redirection
 //        $amount = round($payment_data->payment_amount * 100);
