6c79acfe4964afe48a1d66b20efbdbd5e8ca1078
@m.prokopenko <m.prokopenko@uzum.com>
2025-08-07 01:34:32 +0500

    change phone to user_identity

--- resources/themes/default/web-views/customer-views/auth/partials/_phone.blade.php
+++ resources/themes/default/web-views/customer-views/auth/partials/_phone.blade.php
@@ -4,7 +4,7 @@
         <span class="input-required-icon">*</span>
     </label>
     <input class="form-control text-align-direction phone-input-with-country-picker"
-           type="tel" value="{{ old('phone') }}"
+           type="tel" value="{{ old('user_identity') }}"
            placeholder="{{ translate('enter_phone_number') }}">
-    <input type="hidden" class="country-picker-phone-number w-50 firebase-phone-number" name="phone" readonly>
+    <input type="hidden" class="country-picker-phone-number w-50 firebase-phone-number" name="user_identity" readonly>
 </div>
