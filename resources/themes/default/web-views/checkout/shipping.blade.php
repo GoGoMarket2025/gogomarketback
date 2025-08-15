56c5294b8d1368c1e42629e1c68ff2d84bea2952
gkurbanov <geldierkinovich@gmail.com>
2025-08-15 18:31:38 +0500

    remove from front

--- resources/themes/default/web-views/checkout/shipping.blade.php
+++ resources/themes/default/web-views/checkout/shipping.blade.php
@@ -152,8 +152,7 @@
                                                         </div>
                                                         <div class="col-6">
                                                             <div class="form-group">
-                                                                <label>{{ translate('zip_code')}}
-                                                                    <span class="text-danger">*</span></label>
+                                                                <label>{{ translate('zip_code')}}</label>
                                                                 @if($zip_restrict_status == 1)
                                                                     <select name="zip" class="form-control selectpicker"
                                                                             data-live-search="true"
